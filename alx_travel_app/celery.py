61631166c1273ef43be47656a6727b28acc06ce2
Alpha Infinos <kevinmullerhi@gmail.com>
2025-07-13 15:31:18 +0300

    changes

--- alx_travel_app/celery.py
+++ alx_travel_app/celery.py
@@ -5,9 +5,9 @@ import os
 from celery import Celery
 
 # Set default Django settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app_0x03.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
 
-app = Celery('alx_travel_app_0x03')
+app = Celery('alx_travel_app')
 
 # Load settings from Django settings.py with CELERY_ prefix
 app.config_from_object('django.conf:settings', namespace='CELERY')
